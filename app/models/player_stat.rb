ec5faae984e4682033a54dbb6e7beb582a87fac1
Taeho Kang <tkang1@gmail.com>
2014-02-28 11:06:49 +0900

    refactor player_stat / team_stat

--- app/models/player_stat.rb
+++ app/models/player_stat.rb
@@ -1,15 +1,5 @@
 class PlayerStat < ActiveRecord::Base
-  validates :field_goal_attempted, presence: true
-  validates :field_goal_made, presence: true
-  validates :three_pointer_attempted, presence: true
-  validates :three_pointer_made, presence: true
-  validates :free_throw_attempted, presence: true
-  validates :free_throw_made, presence: true
-  validates :assist, presence: true
-  validates :rebound, presence: true
-  validates :steal, presence: true
-  validates :block, presence: true
-  validates :turnover, presence: true
+  include StatValidator
 
   belongs_to :player
 end
---
+++ app/modules/stat_validator.rb
@@ -0,0 +1,20 @@
+module StatValidator
+  extend ActiveSupport::Concern
+
+  included do
+    validates :field_goal_attempted, presence: true
+    validates :field_goal_made, presence: true
+    validates :three_pointer_attempted, presence: true
+    validates :three_pointer_made, presence: true
+    validates :free_throw_attempted, presence: true
+    validates :free_throw_made, presence: true
+    validates :assist, presence: true
+    validates :rebound, presence: true
+    validates :steal, presence: true
+    validates :block, presence: true
+    validates :turnover, presence: true
+  end
+
+  module ClassMethods
+  end
+end
