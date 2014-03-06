5ca080e9eb3355aa9b2b7eda1cf017ad775d1f43
Taeho Kang <tkang1@gmail.com>
2014-03-07 00:16:03 +0900

    refactor

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,28 +1,28 @@
 class RegistrationsController < Devise::RegistrationsController
   include Devise::Controllers::Helpers
 
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
   prepend_before_filter :require_no_authentication, :only => [:create ]
   before_filter :load_user, only: [:update]
   skip_authorization_check
 
   def create
-    user = User.new(params.require(:user).permit(:email, :password, :name))
-    if user.save
-      render json: user.as_json(authenticatable_salt: user.authenticatable_salt, email: user.email, name: user.name), status: 201
-    else
-      render json: user.errors, status: 400
-    end
+    @user = User.new(params.require(:user).permit(:email, :password, :name))
+    @user.save!
+    render json: @user.as_json(authenticatable_salt: @user.authenticatable_salt, email: @user.email, name: @user.name), status: 201
   end
 
   def update
-    if @user.update_attributes(params.require(:user).permit(:email, :password))
-      render json: @user.as_json(email: @user.email)
-    else
-      render json: @user.errors, status: 400
-    end
+    @user.update_attributes!(params.require(:user).permit(:email, :password))
+    render json: @user.as_json(email: @user.email)
   end
 
   private
+
+  def invalid_record
+    render json: @user.errors, status: 400
+  end
 
   def load_user
     @user = User.find(params[:id])
